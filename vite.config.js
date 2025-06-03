01eaf6d288e52c9e6ac82530e9fc1660e4c7af50
Prashik Ballal <kken9687@gmail.com>
2025-06-03 16:16:08 +0530

    Deploy to GitHub Pages via /docs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/NovelReader/',  // 👈 VERY IMPORTANT
+  base: '/NovelReader/', // Must match your repo name
   plugins: [react()],
 });
